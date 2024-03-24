30a571b43c9baf704f6a1bbf6170bf68d87169fb
cherbourg@fub.fr <cherbourg@fub.fr>
2024-03-24 16:32:34 +0100

    add action to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom/client";
 import Root, {loader as rootLoader,action as rootAction,} from "./routes/root";
 import ErrorPage from "./error-page";
 import Index from "./routes/index";
-import Contact, {loader as contactLoader,} from "./routes/contact";
+import Contact, {loader as contactLoader, action as contactAction} from "./routes/contact";
 import {createBrowserRouter,RouterProvider,} from "react-router-dom";
 import EditContact, {action as editAction,} from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
@@ -17,21 +17,27 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
-      { index: true, element: <Index /> },
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/destroy",
-        action: destroyAction,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+        ],
       },
     ],
   },
